4fd39fc54e73b33b6d83b6e28db9e587af11392e
tushar karwa <tushar.nith494@gmail.com>
2020-05-24 17:59:41 +0530

    updates

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'tusharnavigus.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
